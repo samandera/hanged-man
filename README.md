bcd8d75332b5418d429ac8e189918d469f315ca2
samandera <ela.lach@gmail.com>
2017-06-16 22:10:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# react-sass-starter
+
+Setup
+---
+
+```
+npm install
+```
+
+Sass
+---
+
+It's necessary to have webpack installed globally in order to use watching sass with webpack.
+
+```
+sudo npm install -g webpack --save-dev
+```
+
+Installing Sass loaders
+
+```
+npm install sass-loader node-sass webpack --save-dev
+npm install style-loader
+npm install css-loader
+```
+
+Fonts loader
+---
+
+```
+npm install --save-dev file-loader
+```
+
+
+Compile
+---
+
+```
+npm run compile
+```
+
+
+
+Usage
+---
+
+Watching sass. The process runs in a separate terminal than development server.
+
+```
+webpack --watch
+```
+
+
+Start the development server with this command:
+
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: [{loader : 'file-loader'}]
+      }
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
+};
