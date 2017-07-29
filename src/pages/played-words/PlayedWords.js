aea1d90291c98712543ee6261364f29f3268e6fe
samandera <ela.lach@gmail.com>
2017-07-29 19:00:43 +0200

    add router and PlayedWords page

---
+++ src/pages/played-words/PlayedWords.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PlayedWords extends React.Component {
+  render() {
+    return(
+      <div className="played-words">
+        <h1>Played Words</h1>
+      </div>
+    );
+  }
+}
+export default PlayedWords;
