1c851fb00a19de918a2318407ca1fa6b160cf697
Ela Lach <ela.lach@gmail.com>
2018-10-10 00:42:38 +0200

    test getRandomTitle from PlayableIdiom class

--- src/pages/game/__tests__/createPlayableIdiom.jest.js
+++ src/pages/game/__tests__/createPlayableIdiom.jest.js
@@ -15,3 +15,18 @@ describe("testing calling function within createPlayableIdiom.create", () => {
     expect(spy).toHaveBeenCalled();
   })
 })
+
+describe("test getRandomTitle", () => {
+  const randomTitle = mockedPlayableIdiom.getRandomTitle(titles);
+  it("should be string", () => {
+    expect(typeof randomTitle).resolves.toBe("string");
+  });
+  it("should evalueate to true", () => {
+    expect(randomTitle).resolves.toBeTruthy()
+  });
+  it("should be called with an array passed to create function", () => {
+    const spy = jest.spyOn(mockedPlayableIdiom, "getRandomTitle");
+    mockedCreateIdiom();
+    expect(spy).toHaveBeenCalledWith(titles);
+  })
+})
