7ce58641e9c7ed6ef3c5fc299ae9729fb578cd9c
Ela Lach <ela.lach@gmail.com>
2018-10-15 23:07:17 +0200

    delete setting played words

--- src/pages/store.js
+++ src/pages/store.js
@@ -2,7 +2,6 @@ import { createStore, combineReducers } from 'redux';
 import setWord from './game/reducers/setWord';
 import setMissedLetters from './game/reducers/setMissedLetters';
 import setEndGameState from './game/reducers/showEndGame';
-import setPlayedWords from './game/reducers/setPlayedWords';
 import setFlags from './game/reducers/setFlags';
 import setLoading from './game/reducers/setLoading';
 
@@ -10,7 +9,6 @@ const reducers = combineReducers({
   wordState: setWord,
   missedLettersState: setMissedLetters,
   messageText: setEndGameState,
-  playedWords: setPlayedWords,
   flags: setFlags,
   loadingState: setLoading
 });
