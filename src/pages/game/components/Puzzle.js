64ef2748d94998965a942eccb49cf0acfbfcc3d3
Ela Lach <ela.lach@gmail.com>
2018-10-15 22:31:51 +0200

    delete unused code from Puzzle.js

--- src/pages/game/components/Puzzle.js
+++ src/pages/game/components/Puzzle.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import store from '../../store';
 
-const mapStateToProps = (store) => {
+const mapStateToProps = state => {
   return {
-    word: store.wordState.word
+    word: state.wordState.word
   }
 }
 
