552b165b11731a2aa58afd1a803086e4dafa0e7c
Ela Lach <ela.lach@gmail.com>
2018-11-04 22:25:53 +0100

    remove react-router components

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
-import { createHashHistory, createBrowserHistory } from 'history';
 import Game from './game/Game';
-
-const history = createBrowserHistory();
 
 class Index extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="screen-wrapper">
-          <Route exact path="/" component={Game}/>
-          <div className="grass"/>
-        </div>
-      </Router>
+      <div className="screen-wrapper">
+        <Game/>
+        <div className="grass"/>
+      </div>
     );
   }
 }
